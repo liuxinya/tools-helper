7cbe67e67069dfd5fc4d6b23e1e2d55f0c280928
luhuan01 <luhuan01@baidu.com>
2021-06-17 10:33:52 +0800

    cookie操作同步最新 ACG-CSC-Plat-2310
    Change-Id: Ie2a76fb05cff1dd66e88879466433efdc3d263ca

--- helper/src/cookie.ts
+++ helper/src/cookie.ts
@@ -1,38 +1,45 @@
+/**
+ * @file 操作cookie
+ */
+
+import {isObject} from './type';
 
 /**
  * 获取cookie
  * @param key 只获取某个key对应的value
  */
-
-import {isObject} from './type';
-
 export function getCookie(key?: string) {
     const cookie = getCookieToObj();
     return key ? cookie[key] : cookie;
 }
 
-export function setCookie<T>(key: string, value: T, ms: number = 0) {
-    const valueStr: string = isObject(value) ? JSON.stringify(value) : String(value);
+export function setCookie<T>(key: string, value: T, days: number = 0, isEncode = true) {
+    let valueStr: string = isObject(value) ? JSON.stringify(value) : String(value);
+    valueStr = isEncode ? encodeURIComponent(valueStr) : valueStr;
     let expires: string = '';
-    if (ms > 0) {
-        const d: Date = new Date();
-        d.setTime(d.getTime() + ms);
-        expires = '; expires=' + d.toUTCString();
+    if (days > 0) {
+        const d = new Date();
+        const offset = 8;
+        const utc = d.getTime() + (d.getTimezoneOffset() * 60000);
+        const nd = utc + (3600000 * offset);
+        const exp = new Date(nd);
+        exp.setTime(exp.getTime() + days * 24 * 60 * 60 * 1000);
+        expires = `expires=${exp.toUTCString()};`;
     }
-    document.cookie = key + '=' + escape(valueStr) + expires;
+    document.cookie = `${key}=${valueStr};path=/;${expires};`;
 }
 
 export function delCookie(key: string) {
     document.cookie = key + '=; expires=Thu, 01 Jan 1970 00:00:00 GMT';
 }
 
-function getCookieToObj() {
+function getCookieToObj(isDecode = true) {
     const cookie: string = document.cookie;
     const cookieArr: string[] = cookie.split(';');
     const res: cookieObj = {};
     cookieArr.forEach((item: string) => {
         const temArr = item.split('=');
-        res[temArr[0].trim()] = unescape(temArr[1]);
+        res[temArr[0].trim()] = isDecode ? decodeURIComponent(temArr[1]) : temArr[1];
     });
     return res;
 }
