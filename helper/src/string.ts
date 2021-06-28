04224725688b1d22a5613fb7703b5295db70797f
luhuan01 <luhuan01@baidu.com>
2021-06-28 10:29:42 +0800

    string方法更新 ACG-CSC-Plat-2481
    Change-Id: I6ca437510adf1efcfbef50ae5bb00ecaf7daf1fa

--- helper/src/string.ts
+++ helper/src/string.ts
@@ -5,3 +5,36 @@ export function replaceOfString(str: string, varObj: any, rule = 'i') {
     });
     return res;
 }
+
+/**
+ * 获取字符长度
+ * @param str 需要获取长度的字符串
+ * @returns number 字符长度
+ */
+ export function getByteLen(str: string): number { // 传入一个字符串
+    let len = 0;
+    for (let i = 0; i < str.length; i++) {
+        if (str.charCodeAt(i) > 255) {
+            len += 2;
+        } else {
+            len += 1;
+        }
+    }
+    return len;
+}
+
+// 中文
+export function checkChinese(str: string) {
+    return Boolean(str.match(/[\u4e00-\u9fa5]/g));
+}
+
+// 全角
+export function checkFullWidth(str: string) {
+    return Boolean(str.match(/[\uff00-\uffff]/g));
+}
+
+// 半角
+export function checkHalfWidth(str: string) {
+    // eslint-disable-next-line no-control-regex
+    return Boolean(str.match(/[\u0000-\u00ff]/g));
+}
